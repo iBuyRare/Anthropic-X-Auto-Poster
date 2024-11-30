12bd4438bfec81a0a72a2d322c739160d14cdd47
Rare <ibuyrare@gmail.com>
2024-11-30 12:39:56 -0500

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -1,3 +1,8 @@
+// By: @iBuyRare (X)
+// It’s highly recommended to use environment variables to store sensitive information rather than hardcoding secrets directly into your repository files. However, I’ve chosen to omit this step to keep things simple for newcomers.
+
+//As an alternative, you can download the repository or fork it to your own account and keep it private. This way, your sensitive information will remain secure and hidden from the public.
+
 const APP_KEY = "YOUR_TWITTER_APP_KEY_HERE";
 const APP_SECRET = "YOUR_TWITTER_APP_SECRET_HERE";
 const ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN_HERE";
